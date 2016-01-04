b47f68ee228b005dc8a29904806ffaf1795cb602
Greg Johnson <greg@innerbot.com>
2016-01-04 09:33:24 -0600

    removed temp php testing files

--- vm-files/provisioning/general-server-setup.sh
+++ vm-files/provisioning/general-server-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # update packages db so we can install packages from new repositories
-apt-get update
+apt-get update -y
 # install some utilities we may need
 apt-get install -y htop imagemagick poppler-utils
